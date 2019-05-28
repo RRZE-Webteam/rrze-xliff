033ec31b451338f911a86d97a0aa80b94f4fed24
Florian Brinkmann <kontakt@florianbrinkmann.com>
2019-05-28 11:50:03 +0200

    Spaces for indention

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const path = require( 'path' );
 module.exports = {
   ...defaultConfig,
   entry: {
-	'block-editor-functions': path.resolve( process.cwd(), 'assets/src/js', 'block-editor-functions.js' ),
-	'classic-editor-functions': path.resolve( process.cwd(), 'assets/src/js', 'classic-editor-functions.js' ),
-	'bulk-export-functions': path.resolve( process.cwd(), 'assets/src/js', 'bulk-export-functions.js' ),
+    'block-editor-functions': path.resolve( process.cwd(), 'assets/src/js', 'block-editor-functions.js' ),
+    'classic-editor-functions': path.resolve( process.cwd(), 'assets/src/js', 'classic-editor-functions.js' ),
+    'bulk-export-functions': path.resolve( process.cwd(), 'assets/src/js', 'bulk-export-functions.js' ),
   },
   output: {
-	filename: '[name].js',
-	path: path.resolve( process.cwd(), 'assets/dist/js' ),
+    filename: '[name].js',
+    path: path.resolve( process.cwd(), 'assets/dist/js' ),
   },
 };
